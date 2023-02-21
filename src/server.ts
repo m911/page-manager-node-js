f67f66af60833c2df12f9410a06195279744c033
m911 <mitaka911@gmail.com>
2023-02-21 03:40:54 +0200

    1

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,6 @@ app.post("/", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port: 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port: ${process.env.PORT}`);
 });
