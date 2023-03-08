42e24144bb3a4e94e634a352c03b886901978e0c
m911 <mitaka911@gmail.com>
2023-03-08 05:15:38 +0200

    1

--- src/public/cPanel/new/newPage.js
+++ src/public/cPanel/new/newPage.js
@@ -25,13 +25,18 @@ function attachEventListeners() {
 	newPageForm.addEventListener("submit", async (e) => {
 		e.preventDefault();
 		e.stopPropagation();
-		const urlSlug = await pageService.getPages();
-		const url = urlSlug.map()
+		debugger;
+		const pages = await pageService.getPages();
+		const urls = pages.map((page) => page.url);
+
+		if (urls.contains(e.target[22].value.trim().toLowerCase())) {
+			
+			return;
+		}
 		sessionStorage.setItem("event", JSON.stringify(e));
 		sessionStorage.setItem("event0", JSON.stringify(e.target[0].value));
 		sessionStorage.setItem("event1", JSON.stringify(e.target[1].value));
 		sessionStorage.setItem("event2", JSON.stringify(e.target[2].value));
-		debugger;
 		sessionStorage.setItem("event3", JSON.stringify(e.target[3].value));
 		sessionStorage.setItem("event target", JSON.stringify(e.target));
 	});
